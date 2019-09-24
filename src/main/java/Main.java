dade4306f3474756b38de8ee252675dbd50965ce
marszm <mariusz.szmer@gmail.com>
2019-09-24 16:01:50 +0200

    code refactoring -  now can create new directory depending o the OS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,68 +6,92 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.util.Date;
 
+// zadanie niekompletne, program powinien dzialac caly czas sprawdzajac pojawianie sie nowych plików
+
 public class Main {
 
     public static void main(String[] args) {
 
-        //tworzenie folderow
-        File home = new File("C:\\HOME");
-        File dev = new File("C:\\DEV");
-        File test = new File("C:\\TEST");
-        File count = new File("C:\\home\\count.txt");
+        final String FILENAME = "count.txt";
+        final String HOME = "HOME";
+        final String DEV = "DEV";
+        final String TEST = "TEST";
 
-        if(!home.exists())
-        home.mkdir();
+        String userHomeDirectory = System.getProperty("user.home");
+        System.out.println(userHomeDirectory);
+        String userOS = System.getProperty("os.name").toLowerCase();
+        System.out.println(userOS);
+        if(userOS.contains("windows")) {
 
-        if(!dev.exists())
-        dev.mkdir();
-
-        if(!test.exists())
-        test.mkdir();
-
-        BasicFileAttributes attrs;
-
-        int xmlCounter = 0;
-        int jarCounter = 0;
-
+            String s1 = userHomeDirectory+"\\"+HOME;
+            File file = new File(s1);
+            file.mkdir();
+            System.out.println(s1);
+        }
+        File file = new File(HOME);
         try {
-            //tworzenie pliku do odczytu liczniow
-            count.createNewFile();
-
-            //filtrowanie plikow z rozszerzeniami .xml albp .jar
-            File [] homeFilterjar = home.listFiles((dir, name) -> name.endsWith(".jar"));
-            File[] homeFilterxml = home.listFiles((dir, name) -> name.endsWith(".xml"));
-
-            for(File path: homeFilterjar) {
-                jarCounter++;
-                attrs = Files.readAttributes(path.toPath(), BasicFileAttributes.class);
-                FileTime time = attrs.creationTime();
-                String hh = String.format("%1$tH",new Date(time.toMillis()));
-                int result = Integer.parseInt(hh); //godzina utworzenia pliku
-
-                //jeslo godzina parzysta to do dev, jesli nie to do test
-                if(result % 2 == 0) {
-                    path.renameTo(new File("C:\\DEV\\"+path.getName()));
-                } else {
-                    path.renameTo(new File("C:\\TEST\\"+path.getName()));
-                }
-            }
-            //przenosi wszystkie .xml z home do dev
-            for(File path1: homeFilterxml) {
-                xmlCounter++;
-                path1.renameTo(new File("C:\\DEV\\"+path1.getName()));
-            }
-
-            int totalCounter = xmlCounter + jarCounter;
-
-            //zapisywanie licznikow do pliku count.txt
-            PrintWriter printWriter = new PrintWriter(count);
-            printWriter.println("ile plikow .xml "+xmlCounter);
-            printWriter.println("ile plikow .jar "+jarCounter);
-            printWriter.println("w sumie plikow "+totalCounter);
-            printWriter.close();
-
+            file.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //        jesli katalogi istnieja to niepotrzebnie je zakladamy?
+//        if(!home.exists() || !dev.exists() || !test.exists()) {
+//
+//            home.mkdir();
+//            dev.mkdir();
+//            test.mkdir();
+//
+//        }
+//
+//        BasicFileAttributes attrs;
+//
+//        int xmlCounter = 0;
+//        int jarCounter = 0;
+//
+//                wszystko jest w jednym bloku try
+//        try {
+//            tworzenie pliku do odczytu liczniow
+//            count.createNewFile();
+//
+//            filtrowanie plikow z rozszerzeniami .xml albp .jar
+//            File [] homeFilterjar = home.listFiles((dir, name) -> name.endsWith(".jar"));
+//            File[] homeFilterxml = home.listFiles((dir, name) -> name.endsWith(".xml"));
+//
+//            for(File path: homeFilterjar) {
+//                jarCounter++;
+//                attrs = Files.readAttributes(path.toPath(), BasicFileAttributes.class);
+//                FileTime time = attrs.creationTime();
+//                        uzycie stringa do parsowania godzin
+//                String hh = String.format("%1$tH",new Date(time.toMillis()));
+//                int result = Integer.parseInt(hh); //godzina utworzenia pliku
+//
+//                jeslo godzina parzysta to do dev, jesli nie to do test
+//                if(result % 2 == 0) {
+//                            uzyles File.renameTo(), lepiej byloby uzyc Files.move()
+//                    path.renameTo(new File("C:\\DEV\\"+path.getName()));
+//                } else {
+//                    path.renameTo(new File("C:\\TEST\\"+path.getName()));
+//                }
+//            }
+//            przenosi wszystkie .xml z home do dev
+//            for(File path1: homeFilterxml) {
+//                xmlCounter++;
+//                path1.renameTo(new File("C:\\DEV\\"+path1.getName()));
+//            }
+//
+//            int totalCounter = xmlCounter + jarCounter;
+//
+//            zapisywanie licznikow do pliku count.txt
+//            PrintWriter printWriter = new PrintWriter(count);
+//            printWriter.println("ile plikow .xml "+xmlCounter);
+//            printWriter.println("ile plikow .jar "+jarCounter);
+//            printWriter.println("w sumie plikow "+totalCounter);
+//            printWriter.close();
+//
+//
+//                    catch bez obslugi, tylko zrzut stacktrace
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }}
