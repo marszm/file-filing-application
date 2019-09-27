110f63bf81b6bd8541c466f6f4e8371958c3e39b
marszm <mariusz.szmer@gmail.com>
2019-09-27 15:49:08 +0200

    add spring boot and WatchService works fine for new .jar and .xml files in HOME directory

--- src/main/java/com/example/filefilingapplication/FileFilingApplication.java
+++ src/main/java/com/example/filefilingapplication/FileFilingApplication.java
@@ -120,7 +120,7 @@ public class FileFilingApplication {
             Path pathDev = Paths.get(dev + File.separator + path1.getName());
 
             try {
-                Files.move(pathHome, pathDev);
+                Files.move(pathHome, pathDev, StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -141,59 +141,62 @@ public class FileFilingApplication {
                 System.out.println("Event kind:" + event.kind() + ". File affected: " + event.context() + ".");
                 if(event.kind().toString() == "ENTRY_CREATE") {
 
-                    File[] homeWatchFilterjar = fileHome.listFiles((dir, name) -> name.endsWith(".jar"));
-                    File[] homeWatchFilterxml = fileHome.listFiles((dir, name) -> name.endsWith(".xml"));
-                    for (File path : homeWatchFilterjar) {
-//
-                        jarCounter++;
+                    File[] homeWatchFilter = fileHome.listFiles((dir, name) -> name.endsWith(".jar") || name.endsWith(".xml"));
+                    for (File file : homeWatchFilter) {
+                        System.out.println(file.getName());
+                    }
 
-                        try {
-                            attrs = Files.readAttributes(path.toPath(), BasicFileAttributes.class);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+//                    File[] homeWatchFilterxml = fileHome.listFiles((dir, name) -> name.endsWith(".xml"));
+                    for (File path : homeWatchFilter) {
+                        if (path.getName().endsWith(".jar")) {
+//                        jarCounter++;
+                                System.out.println(path.getName());
+                            try {
+                                attrs = Files.readAttributes(path.toPath(), BasicFileAttributes.class);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
 
-                        FileTime fileTime = attrs.creationTime();
-                        int hh = Integer.parseInt(String.format("%1$tH", new Date(fileTime.toMillis())));
+                            FileTime fileTime = attrs.creationTime();
+                            int hh = Integer.parseInt(String.format("%1$tH", new Date(fileTime.toMillis())));
 
-                        if (hh % 2 == 0) {
+                            if (hh % 2 == 0) {
 
-                            Path pathDev = Paths.get(dev + File.separator + path.getName());
+                                Path pathDev = Paths.get(dev + File.separator + path.getName());
+                                Path pathHome = Paths.get(home + File.separator + path.getName());
+
+                                try {
+//                                Files.move(pathHomeWatch, pathDevWatch, StandardCopyOption.REPLACE_EXISTING);
+                                    Files.move(pathHome, pathDev, StandardCopyOption.REPLACE_EXISTING);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                            } else {
+
+                                Path pathHome = Paths.get(home + File.separator + path.getName());
+                                Path pathTest = Paths.get(test + File.separator + path.getName());
+
+                                try {
+                                    Files.move(pathHome, pathTest, StandardCopyOption.REPLACE_EXISTING);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        } else {
+
+                            xmlCounter++;
                             Path pathHome = Paths.get(home + File.separator + path.getName());
+                            Path pathDev = Paths.get(dev + File.separator + path.getName());
 
                             try {
-//                                Files.move(pathHomeWatch, pathDevWatch, StandardCopyOption.REPLACE_EXISTING);
                                 Files.move(pathHome, pathDev, StandardCopyOption.REPLACE_EXISTING);
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
 
-                        } else {
-
-                            Path pathHome = Paths.get(home + File.separator + path.getName());
-                            Path pathTest = Paths.get(test + File.separator + path.getName());
-
-                            try {
-                                Files.move(pathHome, pathTest, StandardCopyOption.REPLACE_EXISTING);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
                         }
                     }
-
-                    for (File path1 : homeFilterxml) {
-
-                        xmlCounter++;
-                        Path pathHome = Paths.get(home + File.separator + path1.getName());
-                        Path pathDev = Paths.get(dev + File.separator + path1.getName());
-
-                        try {
-                            Files.move(pathHome, pathDev);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
                 }
             }
             key.reset();
@@ -204,5 +207,4 @@ public class FileFilingApplication {
         printWriter.println("w sumie plikow " + totalCounter);
         printWriter.close();
     }
-
 }
